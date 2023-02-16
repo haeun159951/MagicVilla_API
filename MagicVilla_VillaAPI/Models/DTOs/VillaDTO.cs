de116f19b9c484d8f723a56d8cd807f7cd8e712c
Haeun Kim <haeun159951@gmail.com>
2023-02-15 20:48:08 -0500

    added code

---
+++ MagicVilla_VillaAPI/Models/DTOs/VillaDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MagicVilla_VillaAPI.Models.DTOs
+{
+	public class VillaDTO
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
+
---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models
+{
+	public class Villa
+	{
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		public DateTime CreatedDate { get; set; }
+	}
+	
+}
+
