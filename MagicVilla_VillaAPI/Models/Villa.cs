97f54d59584417a445f1ee8b327b4f2d360f6e55
Haeun Kim <haeun159951@gmail.com>
2023-02-17 12:33:21 -0500

    entity

--- MagicVilla_VillaAPI/Models/Villa.cs
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -7,8 +7,14 @@ namespace MagicVilla_VillaAPI.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public DateTime CreatedDate { get; set; }
-	}
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
 	
 }
 
